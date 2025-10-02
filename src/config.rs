27978d0ec6bb5f9a8bc1bc4248ddb58a72c1ba22
Ilshat Sultanov <ilshat@sultanov.team>
2025-10-03 00:39:24 +0300

    Add default config

---
+++ src/config.rs
@@ -0,0 +1,37 @@
+use serde::Deserialize;
+use std::fs;
+use std::path::PathBuf;
+
+#[derive(Debug, Deserialize)]
+pub struct General {
+    pub indent_size: usize,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Clojure {
+    pub indent_size: usize,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub general: General,
+    pub clojure: Option<Clojure>,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self::parse(include_str!("../tsf.toml")).unwrap()
+    }
+}
+
+impl Config {
+    pub fn parse(content: &str) -> anyhow::Result<Self> {
+        let config = toml::from_str(content)?;
+        Ok(config)
+    }
+
+    pub fn load(path: PathBuf) -> anyhow::Result<Self> {
+        let content = fs::read_to_string(path)?;
+        Self::parse(&content)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::path::PathBuf;
 
 use clap::{Parser, Subcommand};
+use config::Config;
+
+pub mod config;
 
 #[derive(Debug, Parser)]
 #[command(name = "tsf")]
@@ -20,15 +23,37 @@ enum Commands {
     /// Check formatting
     Check {
         /// Paths to files or directories
-        path: Vec<String>,
+        path: Vec<PathBuf>,
     },
     /// Fix formatting
     Fix {
         /// Paths to files or directories
-        path: Vec<String>,
+        path: Vec<PathBuf>,
     },
     /// Show version
     Version,
+}
+
+fn load_config(path: Option<PathBuf>) -> Config {
+    match path {
+        // TODO: Configuration file not found - show error message
+        Some(path) => Config::load(path).unwrap_or_default(),
+        None => Config::default(),
+    }
+}
+
+fn check(config: Config, path: Vec<PathBuf>) {
+    println!("Check: {:?}", path);
+    println!("{:?}", config);
+}
+
+fn fix(config: Config, path: Vec<PathBuf>) {
+    println!("Fix: {:?}", path);
+    println!("{:?}", config);
+}
+
+fn show_version() {
+    println!("{}", env!("CARGO_PKG_VERSION"));
 }
 
 fn main() {
@@ -36,15 +61,15 @@ fn main() {
 
     match &cli.command.unwrap() {
         Commands::Check { path } => {
-            println!("Check: {:?}", path);
+            let config = load_config(cli.config);
+            check(config, path.clone())
         }
 
         Commands::Fix { path } => {
-            println!("Fix: {:?}", path);
+            let config = load_config(cli.config);
+            fix(config, path.clone())
         }
 
-        Commands::Version => {
-            println!("{}", env!("CARGO_PKG_VERSION"));
-        }
+        Commands::Version => show_version(),
     }
 }
